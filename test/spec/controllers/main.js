5a24e9c5c8d1173a2ccc0879b84756e578298baf
Steve Duitsman <steveduitsman@aubrey.local>
2016-04-20 21:49:35 -0500

    Initial checkin of yeoman todo app

---
+++ test/spec/controllers/main.js
@@ -0,0 +1,54 @@
+'use strict';
+
+describe('Controller: MainCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('mytodoApp'));
+
+  var MainCtrl,
+      scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    var StubLocalStorage = function(){
+      var backingData = {};
+      var get = function(key){ 
+        return backingData[key];
+      };
+      
+      var set = function(key, value){
+        backingData[key] = value;
+      };
+      
+      return {
+        get: get,
+        set: set
+      };
+    };
+    
+    MainCtrl = $controller('MainCtrl', {
+      $scope: scope,
+      localStorageService: new StubLocalStorage()
+    });
+  }));
+
+  it('should have no items to start', function() {
+    expect(scope.todos.length).toBe(0);
+  });
+  
+  it('should add items to the list', function () {
+    scope.todo = 'Test 1';
+    scope.addTodo();
+    expect(scope.todos.length).toBe(1);
+  });
+  
+  it('should add then remove an item from the list', function (){
+    scope.todo = 'Test 1';
+    scope.addTodo();
+    expect(scope.todos.length).toBe(1);
+    
+    scope.removeTodo(0);
+    expect(scope.todos.length).toBe(0);    
+  });
+});
